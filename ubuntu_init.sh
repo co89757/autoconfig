7c3c96cc8dbec261a3e1b5ebd041ff4131866f6a
Colin Lin <colin.brat@gmail.com>
2016-06-04 23:02:22 -0700

    add more tools to install

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -1,18 +1,97 @@
 #!/bin/sh
 
-## Install dev essential tools 
+### "bash strict mode" quit on any non-zero exit status early  
+## see http://redsymbol.net/articles/unofficial-bash-strict-mode/ 
+set -euo pipefail
+trap "echo 'error: Script failed: see failed command above'" ERR
+export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 
+### Color output setup ###
+END="\e[0m"
+RED="\e[0;31m"
+GREEN="\e[0;32m"
+YELLOW="\e[0;33m"
+BLUE="\e[0;34m"
+
+## Variables 
+if [[ ! -d "$HOME/Downloads" ]]; then
+    mkdir $HOME/Downloads
+fi
+
+
+info()
+{
+if [[ $# -ne 1 ]]; then
+  echo "usage: info message"
+  exit -1
+fi
+ echo -e "${GREEN} INFO: ... $1 ... ${END}\n"
+}
+
+## Install dev essential tools 
+pushd "$HOME/Downloads"
 sudo apt-get update 
-echo "....... Start to install build-essentials ...."
-sudo apt-get install build-essential 
-sudo 
-echo "...... Start to install python-related tools ...."
+## install sublime-text3 
+wget -t 2 https://download.sublimetext.com/sublime-text_build-3114_amd64.deb
+info "... install build-essentials...."
+sudo apt-get install build-essential
+## install checkinstall 
+sudo apt-get install checkinstall
+info "... install cmake ..."
+wget -t 2 https://cmake.org/files/v3.5/cmake-3.5.2.tar.gz -O cmake.tar.gz 
+tar -xzf cmake.tar.gz
+pushd cmake-3.5.2/
+if [[ -z $(which cmake) ]]; then
+    # no prior cmake found 
+    ./bootstrap 
+    make 
+    make install 
+else
+    cmake .
+    make 
+    make install 
+fi
+## back to ~/Downloads
+popd 
+# finish by installing some additional command line tools
+sudo apt-get install -y -q curl rsync tmux zip unzip unrar htop parallel
+sudo apt-get install git 
+git config --global user.name "Colin Lin"
+git config --global user.email "colin.brat@gmail.com"
+git config --global color.ui.auto 
+info ".... install python tools ..."
 sudo apt-get install python3
+sudo apt-get install python-setuptools 
 sudo apt-get install python-pip 
 sudo apt-get install python3-pip 
 sudo apt-get install g++
 
-echo '..... install vim .....'
+info ".... install node.js and npm ..."
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install nodejs 
+
+
+info ".... install vim and config...."
 sudo apt-get install vim
+### pull vimrc 
+scp hl41@178.62.176.117:~/backup/vimrc.backup ~/.vimrc 
+## set up vundle 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+## install all plugins 
+vim +PluginInstall +qall
+## install ack 
+sudo apt-get install ack-grep
+sudo dpkg-divert --local --divert /usr/bin/ack --rename --add /usr/bin/ack-grep
 
+## install bashmark
 
+git clone git://github.com/huyng/bashmarks.git
+pushd bashmarks
+make install 
+## copy bashrc file 
+scp hl41@178.62.176.117:~/backup/bashrc.backup ~/.bashrc
+
+popd 
+
+# back to Downloads
+#
